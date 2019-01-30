da3fb70a932d1c479dd4de088a11fb9262dc736c
Angel Torres <alas4224@gmail.com>
2019-01-30 09:32:45 -0600

    Changed title

--- todo/src/components/Header.js
+++ todo/src/components/Header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render() {
     return (
       <div className="alert alert-light">
-          <h1>To Do App</h1>
+          <h1>React-Redux ToDo App</h1>
       </div>
     )
   }
